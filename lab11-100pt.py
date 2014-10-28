7ffbb6454c15571e8c4f32726b5a63682088f346
Harrmonwuzherrr <Harwat20@yahoo.com>
2014-10-28 15:50:14 -0500

    commit

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -16,21 +16,18 @@ drawpad.grid(row=0, column=0)
 
 # Create the oval
 circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
-direction = 1
+direction = 5
 # Create our animation function
 def animate():
     global direction
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
-    if x2 > drawpad.winfo_width(): 
-        direction = - 1
-    elif x1 < 0:
-        direction = 1
+    if x2 > 800: 
+      drawpad.move(circle,-800,direction)
     #Move our oval object by the value of direction
     drawpad.move(circle,direction,0)
     # Wait for 1 millisecond, then recursively call our animate function
     drawpad.after(1, animate)
 
-# Kick off our animation
 animate()
 root.mainloop()
